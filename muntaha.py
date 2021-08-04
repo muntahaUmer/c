fc1aeb7b599f05270ee359015fcd3688b3f99416
muntahaUmer <88343721+muntahaUmer@users.noreply.github.com>
2021-08-04 06:16:56 -0700

    Delete muntaha.py

--- muntaha.py
+++
@@ -1,4 +0,0 @@
-def personal_details():
-myname, myemail,myusername,mybiostack = "muntaha","muntaha.bsbi589@iiu.edu.pk","muntahaUmer","drug developement anf genomics"
-print("myname: {}\nmyemail: {}\nmyusername: {}\nmybiostack: {}".format(myname, myemail, myusername,mybiostack))
-personal_details()
