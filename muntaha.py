195d498391ad87ce428cf50c0e4d367266c5c1a6
muntahaUmer <88343721+muntahaUmer@users.noreply.github.com>
2021-08-04 11:22:53 -0700

    Update muntaha.py

--- muntaha.py
+++ muntaha.py
@@ -20,7 +20,7 @@ def hamming_distance(myusername, mytwitter_handle):
 def personal_details():
  name, email,slackusername,biostack,twitter_handle, = "muntaha","muntaha.bsbi589@iiu.pk","@muntaha","drug developement anf genomics","@muntaha111"
 
- example_dist = hamming_distance("@muntaha", "@muntaha111")
+ example_dist = hamming_distance("@muntaha", "@sukplmg")
 
  print(example_dist)
  print("name: {}\nemail: {}\nslack_username: {}\nbiostack: {}\ntwitter_handle:{}\nhamming_distance:{}".format(name, email, slackusername,biostack,twitter_handle,example_dist))
